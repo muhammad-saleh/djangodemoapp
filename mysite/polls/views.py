3ce977550d78132e6dec2c16fb8ddd83a8bd9700
Mohamed Saleh <eng.m.ahmed@live.com>
2016-05-23 14:29:53 +0200

    Created a basic question details view

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 from django.template import loader
 from .models import Question
+
 
 
 def index(request):
@@ -11,7 +12,8 @@ def index(request):
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
