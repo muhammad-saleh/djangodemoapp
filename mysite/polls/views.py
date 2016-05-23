eca446cb85f810f05f3a30265251f20e37ed1f4d
Mohamed Saleh <eng.m.ahmed@live.com>
2016-05-23 14:15:53 +0200

    Created the html view for the questions listing page (index)

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Welcome to Polls app")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
